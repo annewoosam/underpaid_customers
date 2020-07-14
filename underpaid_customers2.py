5d636702463acdf99ff7a097ce3af942def34d99
Anne Woo-Sam <annewoosam@gmail.com>
2020-07-14 08:55:45 +0000

    Final Files. Practicing uploading to GitHub. Homework should now be synced. Text file read and values calcualted from.

---
+++ underpaid_customers2.py
@@ -0,0 +1,50 @@
+
+MELON_COST = 1.00
+
+
+def print_payment_status(payment_data_filename):
+    """Calculate cost of melons and determine who has underpaid."""
+
+    payment_data = open(payment_data_filename) # open the file
+
+    # Iterate over lines in file
+    for line in payment_data:
+        # Split line by '|' to get a list of strings
+        order = line.split('|')# because the pipe is the separator. In many cases it will be a tab or a comma
+
+        # Get customer's full name
+        full_name = order[1] #second column in line
+
+        # Split `customer_name` by space (' ') to get
+        # a list of [first_name, last_name].
+        #
+        # Then, assign first name (at index 0) to `customer_first`
+        first_name = full_name.split(" ")[0] #tells to split and take first item in split
+
+        # Get no. of melons in the order and amount customer paid
+        melons_qty = int(order[2]) 
+        amt_paid = float(order[3])# float for currency
+        difference = float(order[3])-float(order[2])
+        # Calculate expected price of customer's order
+        expected_price = melons_qty * MELON_COST
+
+        # Print general payment info
+        print(f"{full_name} paid ${amt_paid:.2f}, expected",
+              f"${expected_price:.2f}", "for a difference of", f"${difference:.2f}")
+        # $ .2f  for currec=ncg and f for avoiding strip space.
+        # Print payment status
+        #
+        # If customer overpaid, print that they've overpaid for their melons. If
+        # customer underpaid, print that they've underpaid for their melons.
+        if expected_price < amt_paid:
+            print(f"{first_name} has overpaid for their melons")
+
+        elif expected_price > amt_paid:
+            print(f"{first_name} has underpaid for their melons.")
+
+    # Close the file
+    payment_data.close()
+
+
+# Call the function
+print_payment_status("customer-orders.txt")
